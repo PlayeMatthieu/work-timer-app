c2999f607a8b471e8b9a1d722f11bdb819df033a
Matthieu Playe <matpl1996@outlook.com>
2023-10-01 13:50:54 +0200

    Setup project structure

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'be.playe.worktimer',
   appName: 'work-timer-app',
   webDir: 'www',
   server: {
